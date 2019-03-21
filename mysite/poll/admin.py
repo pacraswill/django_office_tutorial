7207fbba2c4a05d9e5a9583eaf03f9ffac7133d2
Pacras <791684719@qq.com>
2019-03-21 18:31:53 +0800

    Django 官方入门

---
+++ mysite/poll/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Question, Choice
+# Register your models here.
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
